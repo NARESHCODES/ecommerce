62f2e9e8e85c35f57f837ad8dbce352fa798b0dc
Dwarik <nareshcodes@gmail.com>
2018-08-16 22:42:27 +0545

    started working in dev branch

--- config.php
+++ config.php
@@ -10,3 +10,18 @@ define('_ADMIN_PATH', _ROOT_PATH."/admin");
 
 define('_ROOT_URL','http://'.$_SERVER['HTTP_HOST']);
 define('_ADMIN_URL',_ROOT_URL."/admin");
+
+
+
+/**
+ * Loads the classes automatically
+ *
+ * @param $className
+ */
+function __autoload($className) {
+    $classNames = str_replace('\\', '/', $className);
+    $classFileName = __DIR__ . "/$classNames.php";
+    if(file_exists($classFileName)) {
+        include $classFileName;
+    }
+};
