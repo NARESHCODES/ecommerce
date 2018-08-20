3bf524656e2bbbf35c09cf0606f0a301374ec6ce
Dwarik <nareshcodes@gmail.com>
2018-08-20 21:58:00 +0545

    changed root url in config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ define('_SITE_NAME',        'SmartShop');
 define('_ROOT_PATH',        __DIR__);
 define('_ADMIN_PATH',        _ROOT_PATH . "/admin");
 
-define('_ROOT_URL',         'http://' . $_SERVER['HTTP_HOST']);
+define('_ROOT_URL',         'http://' . $_SERVER['HTTP_HOST'].'/nareshcodes');
 define('_ADMIN_URL',         _ROOT_URL . "/admin");
 
 /**
